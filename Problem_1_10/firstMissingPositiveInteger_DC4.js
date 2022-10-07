66a4b40b83da3b151d347d60153f4498771cd6d3
hazedrops <hazedrops@hotmail.com>
2022-10-06 20:27:25 -0700

    Daily Coding Solution - #4 First Missing Positive Integer

--- Problem_1_10/firstMissingPositiveInteger_DC4.js
+++ Problem_1_10/firstMissingPositiveInteger_DC4.js
@@ -1,28 +1,35 @@
 function findLowestInt(arr) {
-  const low = Math.min(...arr)
-  const high = Math.max(...arr)
+  const result = arr.filter((el) => el === 1)
+  
+  if (result.length === 0) return 1
+  else {
+    const low = Math.min(...arr)
+    const high = Math.max(...arr)
 
-  let intArr = []
+    let intArr = []
 
-  for (let i = low; i <= high; i++) {
-    intArr.push(i)
-  }
-
-  let tempArr = intArr
-
-  arr.forEach((item) => {
-    let idx = intArr.indexOf(item)
-
-    // same items are in both arrays
-    if (idx !== -1) {
-      tempArr.splice(idx, 1)
+    for (let i = low; i <= high; i++) {
+      intArr.push(i)
     }
-  })
 
-  if (tempArr.length === 0) return high + 1
+    let tempArr = intArr
 
-  return Math.min(...intArr.filter((num) => num > 0))
+    arr.forEach((item) => {
+      let idx = intArr.indexOf(item)
+
+      // same items are in both arrays
+      if (idx !== -1) {
+        tempArr.splice(idx, 1)
+      }
+    })
+
+    if (tempArr.length === 0) return high + 1
+
+    return Math.min(...intArr.filter((num) => num > 0))
+  }
 }
 
 // console.log(findLowestInt([3, 4, 1, -1]));
-console.log(findLowestInt([1, 1, 2, 2, 3, 5]))
+// console.log(findLowestInt([1, 1, 2, 2, 3, 5]))
+console.log(findLowestInt([7, 8, 9, 11, 12]))
+
